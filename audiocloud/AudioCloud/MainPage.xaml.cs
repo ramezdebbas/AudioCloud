353cf8920996b412126ff45774700640b687fb15
Rameez <debbas@outlook.com>
2013-05-01 18:36:26 +0500

    Complete App v1.0

---
+++ audiocloud/AudioCloud/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AudioCloud
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private String fileName;
+        MediaCapture mediaCaptureMgr;
+        StorageFile recordStorageFile;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SettingsPane.GetForCurrentView().CommandsRequested += MainPage_CommandsRequested;
+            mediaCaptureMgr = new MediaCapture();
+        }
+
+        void MainPage_CommandsRequested(SettingsPane sender, SettingsPaneCommandsRequestedEventArgs args)
+        {
+            bool afound = false;
+            bool sfound = false;
+            bool pfound = false;
+            foreach (var command in args.Request.ApplicationCommands.ToList())
+            {
+                if (command.Label == "About")
+                {
+                    afound = true;
+                }
+                if (command.Label == "Settings")
+                {
+                    sfound = true;
+                }
+                if (command.Label == "Policy")
+                {
+                    pfound = true;
+                }
+            }
+            if (!afound)
+                args.Request.ApplicationCommands.Add(new SettingsCommand("s", "About", (p) => { cfoAbout.IsOpen = true; }));
+            //if (!sfound)
+            //    args.Request.ApplicationCommands.Add(new SettingsCommand("s", "Settings", (p) => { cfoSettings.IsOpen = true; }));
+            //if (!pfound)
+            //    args.Request.ApplicationCommands.Add(new SettingsCommand("s", "Policy", (p) => { cfoPolicy.IsOpen = true; }));
+            args.Request.ApplicationCommands.Add(new SettingsCommand("privacypolicy", "Privacy policy", OpenPrivacyPolicy));
+        }
+
+        private async void OpenPrivacyPolicy(IUICommand command)
+        {
+            var uri = new Uri("http://www.thatslink.com/privacy-statment/ ");
+            await Launcher.LaunchUriAsync(uri);
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var settings = new MediaCaptureInitializationSettings {StreamingCaptureMode = StreamingCaptureMode.Audio};
+            await mediaCaptureMgr.InitializeAsync(settings);
+        }
+
+        private async void UIElement_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            imgRecording.Visibility = Visibility.Visible;
+            progress.Visibility = Visibility.Visible;
+            Record.IsTapEnabled = false;
+
+
+
+            fileName = DateTime.Today.Year + "_" + DateTime.Today.Month + "_" + DateTime.Today.Day + "_" + DateTime.Today.Hour + "_" + DateTime.Now.Minute + "_" + DateTime.Now.Second + "_" + DateTime.Now.Millisecond + ".m4a";
+            recordStorageFile = await Windows.Storage.KnownFolders.MusicLibrary.CreateFileAsync(fileName);
+
+            Windows.Media.MediaProperties.MediaEncodingProfile recordProfile = null;
+
+            recordProfile = Windows.Media.MediaProperties.MediaEncodingProfile.CreateM4a(Windows.Media.MediaProperties.AudioEncodingQuality.Auto);
+            
+            await mediaCaptureMgr.StartRecordToStorageFileAsync(recordProfile, recordStorageFile);
+        }
+
+        private async void ImgRecording_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+            await mediaCaptureMgr.StopRecordAsync();
+            var stream = await recordStorageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            
+            imgRecording.Visibility = Visibility.Collapsed;
+            progress.Visibility = Visibility.Collapsed;
+            Record.IsTapEnabled = true;
+
+            new Windows.UI.Popups.MessageDialog("File Recorded and saved successfully.").ShowAsync();
+            Frame.Navigate(typeof (RecordedFiles));
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(RecordedFiles));
+        }
+    }
+}
---
+++ audiocloud/AudioCloud/RecordedFiles.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AudioCloud.Common;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace AudioCloud
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class RecordedFiles : AudioCloud.Common.LayoutAwarePage
+    {
+        public IReadOnlyList<StorageFile> LstFiles { get; set; }
+        public RecordedFiles()
+        {
+            this.InitializeComponent();
+            LoadFiles();
+        }
+
+        public async void LoadFiles()
+        {
+           LstFiles= await Windows.Storage.KnownFolders.MusicLibrary.GetFilesAsync();
+           lstView.ItemsSource = LstFiles.OrderByDescending(x=>x.DateCreated);
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        private async void Play_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            var listView = sender as Button;
+            if (listView != null)
+            {
+                var recordStorageFile = LstFiles.First(r => r.DisplayName == listView.Tag.ToString());
+                 
+                if (recordStorageFile != null)
+                {
+                    var stream = await recordStorageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+
+                    playbackElement.AutoPlay = true;
+                    playbackElement.SetSource(stream, recordStorageFile.FileType);
+
+                }
+            }
+            playbackElement.Play(); 
+        }
+
+        private void Stop_OnClick(object sender, RoutedEventArgs e)
+        {
+            playbackElement.Stop();
+        }
+
+        private async void Delete_OnClick(object sender, RoutedEventArgs e)
+        {
+            var listView = sender as Button;
+            if (listView != null)
+            {
+                var recordStorageFile = LstFiles.First(r => r.DisplayName == listView.Tag.ToString());
+
+                if (recordStorageFile != null)
+                {
+                    MessageDialog md = new MessageDialog("Are you sure you want to delete?", "Delete confirmation");
+                    bool? result = null;
+                    md.Commands.Add(
+                       new UICommand("OK", new UICommandInvokedHandler((cmd) => result = true)));
+                    md.Commands.Add(
+                       new UICommand("Cancel", new UICommandInvokedHandler((cmd) => result = false)));
+
+                    await md.ShowAsync();
+                    if (result == true)
+                    {
+                        await recordStorageFile.DeleteAsync(StorageDeleteOption.PermanentDelete);
+                        LstFiles = await Windows.Storage.KnownFolders.MusicLibrary.GetFilesAsync();
+                        lstView.ItemsSource = LstFiles.OrderByDescending(x => x.DateCreated);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
